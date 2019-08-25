8f01ef5d19d02285ac2f95c9bcf00bddbc999703
Hodonou SOUNTON <sounton@gmail.com>
2019-08-25 22:18:53 +0100

    Joi was implemented

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,11 @@
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log('UserController.SignUp is called')
+        // Email & Password. use req.value.body to get body value
+        return res.status(200).json(req.value.body)
+        // console.log('UserController.SignUp is called')
     },
     signIn: async (req, res, next) => {
+        // Generate token
         console.log('UserController.SignIn is called')
     },
     secret: async (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = require('express-promise-router')()
 
+const { validateBody, schemas } = require('../helpers/routeHelpers')
+
 const UserController = require('../controllers/users')
 
 router.route('/signup')
-    .post(UserController.signUp)
+    .post(validateBody(schemas.authSchema), UserController.signUp)
 
 router.route('/signin')
     .post(UserController.signIn)
